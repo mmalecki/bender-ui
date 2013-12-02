e5a6ef1d68ca636209f4d46ae5ee5a6e268c23e7
Maciej Małecki <me@mmalecki.com>
2013-12-02 12:46:53 +0100

    Add version and apps computed observable

--- public/js/app-view-model.js
+++ public/js/app-view-model.js
@@ -5,8 +5,12 @@ var crdtObservableArray = require('knockout-crdt-observable-array');
 var Registration = require('./registration.js');
 var Frontend = require('./frontend.js');
 var Backend = require('./backend.js');
+var App = require('./app.js');
+var version = require('../../package.json').version;
 
 var AppViewModel = function () {
+  var self = this;
+
   this.doc = new Doc({ ttl: false });
   this.shoe = shoe('/bender-crdt');
   this.shoe.pipe(this.doc.createStream()).pipe(this.shoe);
@@ -14,6 +18,19 @@ var AppViewModel = function () {
   this.backends = crdtObservableArray(this.doc.backends, Backend);
   this.frontends = crdtObservableArray(this.doc.frontends, Frontend);
   this.registrations = crdtObservableArray(this.doc.registrations, Registration);
+
+  this.apps = ko.computed(function () {
+    var apps = [];
+    self.registrations().forEach(function (reg) {
+      var id = reg.app + '@' + reg.version;
+      if (apps.indexOf(id) === -1) {
+        apps.push(reg.app + '@' + reg.version);
+      }
+    });
+    return apps;
+  });
+
+  this.version = version;
   // So. Yeah. That was easy.
 };
 
