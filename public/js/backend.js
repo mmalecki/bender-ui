e03477977f4784d9c87a497fcd94767d2bb144bd
Maciej Małecki <me@mmalecki.com>
2013-12-02 18:32:28 +0100

    Make backend properties observable

--- public/js/backend.js
+++ public/js/backend.js
@@ -1,5 +1,22 @@
+var ko = require('knockout');
+
 var Backend = module.exports = function (data) {
-  this.name = data.name;
-  this.app = data.app;
-  this.version = data.version;
+  var self = this;
+
+  this.name = ko.observable(data.name);
+  this.app = ko.observable(data.app);
+  this.version = ko.observable(data.version);
+  this.resource = 'Backend';
+
+  this.appVersion = ko.computed({
+    read: function () {
+      return self.app() + '@' + self.version();
+    },
+    write: function (value) {
+      var split = value.split('@');
+      self.app(split[0]);
+      self.version(split[1]);
+    },
+    owner: self
+  });
 };
